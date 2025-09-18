dfbf76bc95181541eea9646de07f74503db375af
Anika Jairath <ajairath2000@gmail.com>
2025-09-18 11:59:20 +0530

    Update and rename SumOfArr.py to Sum Of Elements In An Array.py
    A basic program to sum elements of an array.

---
+++ Sum Of Elements In An Array.py
@@ -0,0 +1,5 @@
+arr = [1, 2, 3, 4, 5]
+
+sumlist = sum(arr)
+
+print(f"The sum of numbers in the list = {sumlist}")
--- SumOfArr.py
+++
@@ -1,5 +0,0 @@
-arr = [1, 2, 3, 4, 5]
-
-sumlist = sum(arr)
-
-print(f"The sum of numbers in the list = {sumlist}")
