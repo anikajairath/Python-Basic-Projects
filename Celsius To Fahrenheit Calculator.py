1dc882ece172277a1bcd60e402cee7690a4c249e
Anika Jairath <ajairath2000@gmail.com>
2025-09-18 11:53:46 +0530

    Update and rename CelsiusToFahrenheit.py to Celsius To Fahrenheit Calculator.py
    A program that coverts temperature In Celsius to Farenheit.

---
+++ Celsius To Fahrenheit Calculator.py
@@ -0,0 +1,6 @@
+cel = float(input("\nEnter temperature in Celsius: "))
+
+far = (cel*1.8) + 32 #Convert To Farenheit
+
+
+print(f"\nThe Temperature in Fahrenheit is: {far:.2f}")
--- CelsiusToFahrenheit.py
+++
@@ -1,5 +0,0 @@
-cel = float(input("\nEnter temperature in Celsius: "))
-
-far = (cel*1.8) + 32
-
-print(f"\nThe Temperature in Fahrenheit is: {far:.2f}")
